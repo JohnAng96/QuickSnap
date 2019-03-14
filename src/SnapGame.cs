dcdb941910109fb91f6b5d46572ccaed0d6e0c3e
JohnAng96 <jjpandasnow@gmail.com>
2019-03-14 15:17:07 +0800

    UI card placement

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -59,7 +59,8 @@ namespace CardGames
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
+				//SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
+				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 570, 225);
 			}
 			else
 			{
@@ -67,7 +68,8 @@ namespace CardGames
 			}
 
 			// Draw the back of the cards... to represent the deck
-			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 160, 50);
+			//SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 160, 50);
+			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 200, 225);
 
 			//Draw onto the screen
 			SwinGame.RefreshScreen(60);
